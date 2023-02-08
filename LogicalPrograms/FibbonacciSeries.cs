14ac63f54551e51ae41cbe766edfd0a89abe6698
vaibhav <vaibhavmehta524@gmail.com>
2023-02-09 02:34:31 +0530

    Fibbonacci Series Program

---
+++ LogicalPrograms/FibbonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    internal class FibbonacciSeries
+    {
+        public static void FibNumber()
+        {
+            int i = 0, j = 1, k = 2, num = 0;
+            Console.WriteLine("Enter the number:");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Fibbonacci series:" + i);
+            Console.WriteLine("Fibbonacci series:" + j);
+            for (k = 2; k < num; k++)
+            {
+                k = i + j;
+                Console.WriteLine("Fibbonacci series:" + k);
+                i = j;
+                j = k;
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the logical programs");
+            Console.WriteLine("Choose option from below");
+            Console.WriteLine("1.FibbonacciSeries");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    FibbonacciSeries.FibNumber();
+                    break;
+            }
+         }
+    }
+}
